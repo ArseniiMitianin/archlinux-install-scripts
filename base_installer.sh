f7ef2c8dd12ebd7ace5c07be6e4d4af3803d4cf7
ArseniiMitianin <arsenii.mitianin@yandex.ru>
2021-10-03 19:25:58 -0400

    Added an EOL, so that GitHub doesn't complain

--- base_installer.sh
+++ base_installer.sh
@@ -87,4 +87,4 @@ case $desktop in
     kw) launch kde.sh wayland ;;
 esac
 
-green "\nInstallation is complete!\nType in 'umount -R /mnt', and reboot.\n" 
+green "\nInstallation is complete!\nType in 'umount -R /mnt', and reboot.\n"
